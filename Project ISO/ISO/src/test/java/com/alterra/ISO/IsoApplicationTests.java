cedca94533cee2a9bc94e0e470e28d85c35705ae
Alzetar <alzetarosdyana04@gmail.com>
2022-04-27 17:30:19 +0700

    mvc

---
+++ Project ISO/ISO/src/test/java/com/alterra/ISO/IsoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alterra.ISO;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IsoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
