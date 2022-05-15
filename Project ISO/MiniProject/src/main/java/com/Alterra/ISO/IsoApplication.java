1a5adfd6de92151c2a1397d6704850ab5ccd6b06
Alzetar <alzetarosdyana04@gmail.com>
2022-05-15 11:20:40 +0700

    Swagger

--- Project ISO/MiniProject/src/main/java/com/Alterra/ISO/IsoApplication.java
+++ Project ISO/MiniProject/src/main/java/com/Alterra/ISO/IsoApplication.java
@@ -1,9 +1,11 @@
 package com.Alterra.ISO;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition
 public class IsoApplication {
 
 	public static void main(String[] args) {
